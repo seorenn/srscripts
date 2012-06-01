730517da30aec60b769d630e1985d961a3550988
Heeseung Seo <hirenn@websync.co.kr>
2012-06-01 16:53:58 +0900

    added inline options for commit message

--- gca.sh
+++ gca.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 echo ''
 echo -ne "\033[32mCOMMIT MESSAGE: \033[0m"
-read commit_message
-len=${#commit_message}
-if [ "$len" -lt "1" ]; then
-    echo -e "\033[32mCANCLED\033[0m"
-    echo ""
-    exit -1
+cmdopt=$1
+lenopt=${#cmdopt}
+if [ "$lenopt" -lt "1" ]; then
+    read commit_message
+    len=${#commit_message}
+    if [ "$len" -lt "1" ]; then
+        echo -e "\033[32mCANCLED\033[0m"
+        echo ""
+        exit -1
+    fi
+else
+    commit_message=$cmdopt
 fi
 echo ""
 echo -e "\033[32m# TRYING TO STAGE ALL NEW FILES AND CHANGES...\033[0m"
