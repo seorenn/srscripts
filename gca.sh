fc346b189879ae4ee8fda66421661514e7d01b06
Heeseung Seo <hirenn@websync.co.kr>
2012-06-19 12:34:35 +0900

    updated messages

--- gca.sh
+++ gca.sh
@@ -5,21 +5,20 @@ if ! git ls-files >& /dev/null; then
 fi
 cmdopt=$1
 lenopt=${#cmdopt}
-if [ "$lenopt" -lt "1" ]; then
-    echo ''
-    echo -ne "\033[32mCOMMIT MESSAGE: \033[0m"
-    read commit_message
-    len=${#commit_message}
-    if [ "$len" -lt "1" ]; then
-        echo -e "\033[32mCANCLED\033[0m"
-        echo ""
-        exit -1
-    fi
-else
-    commit_message=$cmdopt
-fi
-echo ""
 echo -e "\033[32m# TRYING TO STAGE ALL NEW FILES AND CHANGES...\033[0m"
 git add .
-git commit -am "${commit_message}"
-echo ""
+if [ "$lenopt" -lt "1" ]; then
+    git commit -a
+    #echo ''
+    #echo -ne "\033[32mCOMMIT MESSAGE: \033[0m"
+    #read commit_message
+    #len=${#commit_message}
+    #if [ "$len" -lt "1" ]; then
+    #    echo -e "\033[32mCANCLED\033[0m"
+    #    echo ""
+    #    exit -1
+    #fi
+else
+    commit_message=$cmdopt
+    git commit -am "${commit_message}"
+fi
--- gs.sh
+++ gs.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# check remote repo
+GIT_REMOTES=`git remote show`
 # Git status with not-pushed commits
 if ! git ls-files >& /dev/null; then
     echo "NOT GIT REPOSITORY. SKIP."
     exit -1
 fi
 echo ''
-echo -e "\033[32m# STATUS\033[0m"
+echo -e "\033[32m# STATUS OF CURRENT BRANCH\033[0m"
 git status
 echo ''
-echo -e "\033[32m# FETCHED LOG\033[0m"
-git log master..origin/master --pretty=oneline --graph --abbrev-commit --decorate -n 4
-echo ''
+if [[ $GIT_REMOTES ]]; then
+    echo -e "\033[32m# FETCHED LOG WITH origin/master\033[0m"
+    git log master..origin/master --pretty=oneline --graph --abbrev-commit --decorate -n 4
+    echo ''
+fi
 if [[ `git remote` ]]; then
-    echo -e "\033[32m# UNPUSHED COMMITS\033[0m"
+    echo -e "\033[32m# UNPUSHED COMMITS TO origin/master\033[0m"
     git log origin/master..HEAD --pretty=oneline --graph --abbrev-commit --decorate -n 4
     echo ''
 fi
